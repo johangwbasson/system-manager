544ea479d91df43b654e9567571ec181f17380ac
Johan Basson <johangwbasson@gmail.com>
2020-12-11 15:51:15 +0200

    Added more logging

--- sm.py
+++ sm.py
@@ -26,23 +26,36 @@ def writeConfig(cfg):
         print("Error: Unable to write configuration to file")
 
 def installPackage(pkg):
-    proc = subprocess.Popen(['sudo', 'pacman', '-S', pkg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(['sudo', 'pacman', '-Qi', pkg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (stdout, stderr) = proc.communicate()
-    if 'error: target not found:' in stderr.decode('utf-8'):
-        proc = subprocess.Popen(['yay', '-S', pkg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if 'Could not find all required packages' in stderr.decode('utf-8'):
-            print("Error: Package %s not found" % pkg)
-            return False
-    print("Package %s installed" % pkg)
-    return True
+    if 'was not found' in stderr.decode('utf-8'):
+    
+        print("Installing package %s" % pkg)
+        proc = subprocess.Popen(['sudo', 'pacman', '-S', pkg, '--noconfirm'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdout, stderr) = proc.communicate()
+        if 'error: target not found:' in stderr.decode('utf-8'):
+        
+            proc = subprocess.Popen(['yay', '-S', pkg, '--noconfirm'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            (stdout, stderr) = proc.communicate()
+            if 'Could not find all required packages' in stderr.decode('utf-8'):
+                print("Error: Package %s not found" % pkg)
+                return False
+
+        print("Package %s installed" % pkg)
+        return True
+
+    print("Package %s is already installed" % pkg)
+    return False
 
 def removePackage(pkg):
-    result = subprocess.run(['sudo', 'pacman', '-R', pkg], stdout=subprocess.PIPE)
+    print("Removing package %s" % pkg)
+    result = subprocess.run(['sudo', 'pacman', '-R', pkg, '--noconfirm'], stdout=subprocess.PIPE)
     if 'error: target not found:' in result.stdout.decode('utf-8'):
-        result = subprocess.run(['yay', '-R', pkg], stdout=subprocess.PIPE)
+        result = subprocess.run(['yay', '-R', pkg, '--noconfirm'], stdout=subprocess.PIPE)
         if 'error: target not found:' in result.stdout.decode('utf-8'):
             print("Error: Package %s is not installed" % pkg)
             return False
+    print("Package %s removed" % pkg)
     return True
 
 
